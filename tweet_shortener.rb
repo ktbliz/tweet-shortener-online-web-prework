5281612addf8e5cdbd588023be5402d544233715
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 23:23:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,4 +29,4 @@ def word_substituter(dictionary, tweet)
   new_tweet
 end 
 
-word_substituter(dictionary, )
+word_substituter(dictionary, "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.")
