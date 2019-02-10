91e8c3f6aeb10949a549437712d5a8690278a163
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 23:07:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,16 @@
 def dictionary 
-
+  dictionary_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2", 
+    "too" => "2", 
+    "for" => "4",
+    "four" => "4",
+    "be" => "b", 
+    "you" => "u", 
+    "at" => "@", 
+    "and" => "&"
+  }
 end 
 
 def word_substituter(tweet)
